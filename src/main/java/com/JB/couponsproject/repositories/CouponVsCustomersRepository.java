3f4d157f63688a17490e4970c342922aaece5c6e
Nitay Caspi <NitayCaspi12@gmail.com>
2022-04-05 19:26:10 +0300

    add id

--- src/main/java/com/JB/couponsproject/repositories/CouponVsCustomersRepository.java
+++ src/main/java/com/JB/couponsproject/repositories/CouponVsCustomersRepository.java
@@ -5,5 +5,5 @@ import com.JB.couponsproject.entities.CouponVsCustomers;
 import com.JB.couponsproject.entities.CustomerEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface CouponVsCustomersRepository extends JpaRepository<CustomerEntity, CouponEntity> {
+public interface CouponVsCustomersRepository extends JpaRepository<CouponVsCustomers, Long> {
 }
