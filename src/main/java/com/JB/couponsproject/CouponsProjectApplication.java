8391a771a90e31715790d73bb90c1fa2fbefbbbe
Nitay Caspi <NitayCaspi12@gmail.com>
2022-04-05 19:24:00 +0300

    add mock data inserter

--- src/main/java/com/JB/couponsproject/CouponsProjectApplication.java
+++ src/main/java/com/JB/couponsproject/CouponsProjectApplication.java
@@ -1,4 +1,6 @@
 package com.JB.couponsproject;
+
+import com.JB.couponsproject.util.MockDataInserter;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
@@ -8,6 +10,8 @@ public class CouponsProjectApplication {
 
 	public static void main(String[] args) {
 		ApplicationContext ctx = SpringApplication.run(CouponsProjectApplication.class, args);
+		final MockDataInserter mockDataInserter = ctx.getBean(MockDataInserter.class);
+		mockDataInserter.insert();
 	}
 
 }
