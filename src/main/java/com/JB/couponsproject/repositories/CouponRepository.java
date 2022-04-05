9480f7635055c2a291ffd238732b012accca3da9
Nitay Caspi <NitayCaspi12@gmail.com>
2022-04-05 19:23:14 +0300

    add company coupons methods

--- src/main/java/com/JB/couponsproject/repositories/CouponRepository.java
+++ src/main/java/com/JB/couponsproject/repositories/CouponRepository.java
@@ -14,14 +14,26 @@ public interface CouponRepository extends JpaRepository<CouponEntity,Long> {
     List<CouponEntity> findByPriceLessThan(double maxPrice);
 
     @Query(value = "SELECT * FROM coupons AS c JOIN coupon_vs_customers AS cc ON cc.coupon_id = c.id WHERE " +
-            "cc.customer_id = ?1",nativeQuery = true)
+                   "cc.customer_id = ?1", nativeQuery = true)
     List<CouponEntity> getCustomerCoupons(long customerID);
 
     @Query(value = "SELECT * FROM coupons AS c JOIN coupon_vs_customers AS cc" +
-            " ON c.id = cc.coupon_id WHERE cc.customer_id = ?1", nativeQuery = true)
+                   " ON c.id = cc.coupon_id WHERE cc.customer_id = ?1", nativeQuery = true)
     List<CouponEntity> getCustomerCouponsByCategory(long customerID, Category category);
 
     @Query(value = "SELECT * FROM coupons AS c JOIN coupon_vs_customers AS cc" +
-            " ON c.id = cc.coupon_id WHERE cc.customer_id = ?1", nativeQuery = true)
-    List<CouponEntity> findCustomerCouponsByPriceLessThan(long customerID,double maxPrice);
+                   " ON c.id = cc.coupon_id WHERE cc.customer_id = ?1", nativeQuery = true)
+    List<CouponEntity> findCustomerCouponsByPriceLessThan(long customerID, double maxPrice);
+
+    @Query(value = "SELECT * FROM coupons AS c WHERE " +
+                   "c.company_id = ?1", nativeQuery = true)
+    List<CouponEntity> getCompanyCoupons(long companyID);
+
+    @Query(value = "SELECT * FROM coupons AS c WHERE " +
+                   "c.company_id = ?1", nativeQuery = true)
+    List<CouponEntity> getCompanyCouponsByCategory(long companyID, Category category);
+
+    @Query(value = "SELECT * FROM coupons AS c WHERE " +
+                   "c.company_id = ?1", nativeQuery = true)
+    List<CouponEntity> getCompanyCouponsByPriceLessThan(long companyID, double maxPrice);
 }
