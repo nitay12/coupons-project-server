23d314385d394444b30fd7adf56010706c7514c6
Lior Cohen <lior1cc@gmail.com>
2022-04-05 23:03:21 +0300

    complete repositories: Company, Customer - testing required

--- src/main/java/com/JB/couponsproject/repositories/CompanyRepository.java
+++ src/main/java/com/JB/couponsproject/repositories/CompanyRepository.java
@@ -1,9 +1,31 @@
 package com.JB.couponsproject.repositories;
+
 import com.JB.couponsproject.entities.CompanyEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.ArrayList;
 
 public interface CompanyRepository extends JpaRepository<CompanyEntity,Long> {
 
+    CompanyEntity getDetailById(long customerId);
+
+    CompanyEntity getDetailByEmail(String email);
+
     boolean existsByEmailAndPassword(String email,String password);
+
+    boolean existsByEmail(String email);
+
+    boolean existsByName(String name);
+
+    ArrayList<CompanyEntity> getCompaniesArrayList();
+
+    void deleteByID(long companyId);
+
+    @Modifying(clearAutomatically = true)
+    @Query("UPDATE companies SET password = :password WHERE id = :companyId")
+    public void updateCompany(@Param("password") int password, @Param("companyId") int companyId);
 
 }
--- src/main/java/com/JB/couponsproject/repositories/CustomerRepository.java
+++ src/main/java/com/JB/couponsproject/repositories/CustomerRepository.java
@@ -1,12 +1,27 @@
 package com.JB.couponsproject.repositories;
 
-import com.JB.couponsproject.entities.CouponEntity;
 import com.JB.couponsproject.entities.CustomerEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
-
-import java.util.List;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface CustomerRepository extends JpaRepository<CustomerEntity,Long> {
 
+    CustomerEntity getDetailById(long customerId);
+
+    CustomerEntity getDetailByEmail(String email);
+
     boolean existsByEmailAndPassword(String email,String password);
+
+    boolean existsByEmail(String email);
+
+    @Modifying(clearAutomatically = true)
+    @Query("UPDATE customers SET first_name = :firstname, last_name = :lastname, email = :email, password = :password WHERE id = :id")
+    public void updateCustomer(@Param("firstname") String firstname, @Param("lastname") String lastname, @Param("email") String email, @Param("password") int password, @Param("id") long id);
+
+    void deleteByID(long customerId);
+
+
+
 }
