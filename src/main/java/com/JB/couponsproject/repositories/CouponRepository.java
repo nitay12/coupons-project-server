5dd07c5e6ed6d8fecedfbbbb49cdb703cb0e4ae2
Lior Cohen <lior1cc@gmail.com>
2022-04-06 21:45:59 +0300

    add annotations

--- src/main/java/com/JB/couponsproject/repositories/CouponRepository.java
+++ src/main/java/com/JB/couponsproject/repositories/CouponRepository.java
@@ -4,9 +4,11 @@ import com.JB.couponsproject.entities.CouponEntity;
 import com.JB.couponsproject.enums.Category;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
+@Repository
 public interface CouponRepository extends JpaRepository<CouponEntity,Long> {
 
     List<CouponEntity> findByCategory(Category category);
--- src/main/java/com/JB/couponsproject/repositories/CustomerRepository.java
+++ src/main/java/com/JB/couponsproject/repositories/CustomerRepository.java
@@ -2,10 +2,9 @@ package com.JB.couponsproject.repositories;
 
 import com.JB.couponsproject.entities.CustomerEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Modifying;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface CustomerRepository extends JpaRepository<CustomerEntity,Long> {
 
     CustomerEntity getDetailById(long customerId);
