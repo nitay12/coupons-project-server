10b925b7a95f8b8901edd170c88abb5c6ad1491f
Nitay Caspi <NitayCaspi12@gmail.com>
2022-04-10 12:28:30 +0300

    create coupon mapping method

--- src/main/java/com/JB/couponsproject/util/ObjectMappingUtil.java
+++ src/main/java/com/JB/couponsproject/util/ObjectMappingUtil.java
@@ -1,4 +1,19 @@
 package com.JB.couponsproject.util;
 
+import com.JB.couponsproject.dto.CouponDto;
+import com.JB.couponsproject.entities.CouponEntity;
+
 public class ObjectMappingUtil {
+    public static CouponEntity couponDtoToEntity(CouponDto couponDto){
+        return new CouponEntity(
+                couponDto.getCategory(),
+                couponDto.getTitle(),
+                couponDto.getDescription(),
+                couponDto.getStartDate(),
+                couponDto.getEndDate(),
+                couponDto.getAmount(),
+                couponDto.getPrice(),
+                couponDto.getImage()
+        );
+    }
 }
