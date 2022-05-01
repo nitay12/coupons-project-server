4fec6a68a24d660cebe8bbc08711e4a2dff7bc0e
Nitay Caspi <NitayCaspi12@gmail.com>
2022-05-01 11:42:46 +0300

    add tests

--- src/main/java/com/JB/couponsproject/CouponsProjectApplication.java
+++ src/main/java/com/JB/couponsproject/CouponsProjectApplication.java
@@ -2,11 +2,13 @@ package com.JB.couponsproject;
 
 import com.JB.couponsproject.dailyjob.DailyJob;
 import com.JB.couponsproject.dto.CouponDto;
+import com.JB.couponsproject.entities.CompanyEntity;
 import com.JB.couponsproject.entities.CouponEntity;
 import com.JB.couponsproject.enums.Category;
 import com.JB.couponsproject.exceptions.ApplicationException;
 import com.JB.couponsproject.repositories.CouponRepository;
 import com.JB.couponsproject.services.CompanyService;
+import com.JB.couponsproject.util.ObjectMappingUtil;
 import lombok.Data;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -27,7 +29,9 @@ public class CouponsProjectApplication {
 			//Tests
 			//Login test
 			companyService.login("company2@email.com", "123456");
+			System.out.println("WELCOME "+ companyService.getLoggedInCompany().getName().toUpperCase());
 			//Add coupon test
+			System.out.println("Adding coupon...");
 			final CouponDto testCouponDto = new CouponDto(
 					Category.ELECTRICITY,
 					"test title",
@@ -39,14 +43,26 @@ public class CouponsProjectApplication {
 					"url"
 			);
 			Long newCouponId = companyService.addCoupon(testCouponDto);
-			CouponEntity newCoupon = couponRepository.getById(newCouponId);
+			System.out.println("Coupon added (id:"+newCouponId+")");
+			CouponEntity newCoupon = couponRepository.findById(newCouponId).get();
 			//Update coupon test
-			testCouponDto.setId(newCouponId);
-			testCouponDto.setDescription("updated desc");
-			companyService.updateCoupon(testCouponDto);
+			System.out.println("Updating coupon...");
+			newCoupon.setDescription("updated desc");
+			final CouponDto newCouponDto = ObjectMappingUtil.couponEntityToCouponDto(newCoupon);
+			companyService.updateCoupon(newCouponDto);
+			System.out.println("Updated coupon:");
 			System.out.println(couponRepository.findById(newCouponId).get().toString());
 			// Delete coupon test
-			companyService.deleteCoupon(1L);
+			System.out.println("Deleting coupon...");
+			companyService.deleteCoupon(2L);
+			System.out.println("Coupon deleted");
+			//Get company coupons test
+			System.out.println("All company coupons");
+			System.out.println(companyService.getCompanyCoupons());
+			System.out.println("All company coupons from category (ELECTRICITY)");
+			System.out.println(companyService.getCompanyCoupons(Category.ELECTRICITY));
+			System.out.println("All company coupons up to 500 (max price)");
+			System.out.println(companyService.getCompanyCoupons(500d));
 		} catch (ApplicationException e) {
 			e.printStackTrace();
 		}
