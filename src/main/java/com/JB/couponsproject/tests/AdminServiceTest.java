ab421d3569e80e0b4a7d1b0478a764981b1de7c3
Lior Cohen <lior1cc@gmail.com>
2022-05-14 16:35:17 +0300

    Completed admin service test

--- src/main/java/com/JB/couponsproject/tests/AdminServiceTest.java
+++ src/main/java/com/JB/couponsproject/tests/AdminServiceTest.java
@@ -1,7 +1,9 @@
 package com.JB.couponsproject.tests;
 
 import com.JB.couponsproject.dto.CompanyDto;
+import com.JB.couponsproject.dto.CustomerDto;
 import com.JB.couponsproject.entities.CompanyEntity;
+import com.JB.couponsproject.entities.CustomerEntity;
 import com.JB.couponsproject.login.LoginManager;
 import com.JB.couponsproject.repositories.CompanyRepository;
 import com.JB.couponsproject.services.AdminService;
@@ -11,6 +13,8 @@ import org.slf4j.LoggerFactory;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.core.annotation.Order;
 import org.springframework.stereotype.Component;
+
+import java.util.List;
 
 @Component
 @Order(2)
@@ -25,11 +29,58 @@ public class AdminServiceTest implements CommandLineRunner {
         //Add company test
         logger.info("Adding new company");
         final CompanyEntity company = adminService.createCompany(CompanyEntity.builder().email("COMPANY@TEST.COM").name("Test Company LTD").password(123456).build());
-        CompanyDto companyDto = companyRepository.findByName("Test Company LTD").get(0);
         logger.info("Company added, "+company.toString());
+        logger.info("Get Company by: name, id, email");
+        CompanyDto companyDto = adminService.getCompanyByName("Test Company LTD");
+        logger.info("Get Company by: name\n" + companyDto.toString());
+        long id = companyDto.getId();
+        logger.info("Resetting company.");
+        companyDto  = null;
+        companyDto = adminService.getCompanyById(id);
+        logger.info("Get Company by: id\n" + companyDto.toString());
+        logger.info("Resetting company.");
+        companyDto  = null;
+        companyDto = adminService.getCompanyByEmail("COMPANY@TEST.COM");
+        logger.info("Get Company by: email\n" + companyDto.toString());
+        //Update company test
         logger.info("Updating company");
         company.setName("Test Update name");
         adminService.updateCompany(companyDto);
         logger.info("company updated: "+adminService.getCompanyById(company.getId()).toString());
+
+        //Add customer test
+        logger.info("Adding new customer");
+        final CustomerEntity customer = adminService.createCustomer(CustomerEntity.builder().email("CUSTOMER@TEST.COM").firstName("FTEST").lastName("LTEST").password(123456).build());
+        logger.info("Customer added, "+customer.toString());
+        //Get customer tests
+        CustomerDto customerDto = adminService.getCustomerByEmail("CUSTOMER@TEST.COM");
+        logger.info("Get Customer by: email\n" + customerDto.toString());
+        id = customerDto.getId();
+        logger.info("Resetting customer.");
+        customerDto  = null;
+        customerDto = adminService.getCustomerById(id);
+        logger.info("Get Customer by: Id\n" + customerDto.toString());
+        //Update customer test
+        logger.info("Updating this customer");
+        customerDto.setFirstName("First");
+        customerDto.setLastName("Last");
+        adminService.updateCustomer(customerDto);
+        logger.info("Customer updated, "+customerDto.toString());
+
+        //Get All
+        List<CompanyEntity> companies = adminService.getAllCompanies();
+        logger.info("Get all companies :\n"+ companies.toString());
+        List<CustomerEntity> customers = adminService.getAllCustomers();
+        logger.info("Get all customers :\n"+ customers.toString());
+        
+        //delete company test
+        logger.info("About to delete company: "+companyDto.getName());
+        adminService.deleteCompany(companyDto);
+        logger.info("Deleted company: "+companyDto.getName());
+        //Delete customer test
+        logger.info("About to delete customer: "+customerDto.getEmail());
+        adminService.deleteCustomer(customerDto);
+        logger.info("Deleted customer: "+customerDto.getEmail());
+
     }
 }
