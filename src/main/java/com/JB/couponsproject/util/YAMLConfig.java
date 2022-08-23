02afe894188062290cd0a0ddac8f9a3b31d9f2de
Lior Cohen <lior1cc@gmail.com>
2022-08-23 16:37:54 +0300

    added YAMLCongif and JWTutil files
    ${JWT_SECRET} been added as local environment value connected to YMALConfig file

---
+++ src/main/java/com/JB/couponsproject/util/YAMLConfig.java
@@ -0,0 +1,13 @@
+package com.JB.couponsproject.util;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@EnableConfigurationProperties
+@ConfigurationProperties
+public class YAMLConfig {
+    public static String jwtsecret;
+
+}
