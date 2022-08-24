08db72b124dec0c2a36d12139e7f42993e9e2d1b
Lior Cohen <lior1cc@gmail.com>
2022-08-25 01:16:50 +0300

    Update LoginManager.java

--- src/main/java/com/JB/couponsproject/login/LoginManager.java
+++ src/main/java/com/JB/couponsproject/login/LoginManager.java
@@ -43,4 +43,6 @@ public class LoginManager {
             throw new ApplicationException(); //check for other options
         }
     }
+
+    //ToDo: Signup via adminService to either company or customer entity, and return JwtWrapper token.
 }
