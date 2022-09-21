de1e919cf4aa12e1000f681a91a33a580bbf8763
Nitay Caspi <NitayCaspi12@gmail.com>
2022-09-21 14:49:05 +0300

    add cors config

--- src/main/java/com/JB/couponsproject/conf/SecurityConfig.java
+++ src/main/java/com/JB/couponsproject/conf/SecurityConfig.java
@@ -16,13 +16,13 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
+        http.cors();
         http.
                 csrf().disable()
                 .authorizeRequests()
                 .antMatchers("/auth/login").permitAll()
                 .anyRequest().authenticated();
         http.addFilterBefore(jwtRequestFilter, UsernamePasswordAuthenticationFilter.class);
-
     }
 
 }
--- src/main/java/com/JB/couponsproject/controllers/AuthController.java
+++ src/main/java/com/JB/couponsproject/controllers/AuthController.java
@@ -5,16 +5,15 @@ import com.JB.couponsproject.login.LoginManager;
 import com.JB.couponsproject.dto.ClientLoginDto;
 import com.JB.couponsproject.security.JwtWrapper;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("auth")
 @RequiredArgsConstructor
+@CrossOrigin
 public class AuthController {
     private final LoginManager loginManager;
+
     @PostMapping("login")
     public JwtWrapper login(@RequestBody ClientLoginDto client) throws ApplicationException {
         return loginManager.jwtLogin(client.getUserType(), client.getEmail(), client.getPassword());
